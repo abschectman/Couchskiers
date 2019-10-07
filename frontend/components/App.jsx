97e52c0fb3c6c0cfe79a0d231d3d5ea6486d5c26
abschectman <abschectman@gmail.com>
2019-10-07 11:06:45 -0400

    Added edit page and backend routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import SignUpConainer from "./session/signup_container"
 import Login from "./modal/modal_container"
 import Dash from "./dash/dash_container"
+import Edit from "./user/edit_user_container"
 import {AuthRoute, ProtectedRoute} from "../util/route_util"
 const App = () => (
   <div>
     <AuthRoute path="/signup" component={SignUpConainer} />
     <AuthRoute path="/signup" component={Login} />
+    <ProtectedRoute path="users/banana/:userId" component={Edit} />
     <ProtectedRoute path="/users/:userId" component={Dash} />
   </div>
 );
---
+++ frontend/components/user/edit_user_container.js
@@ -0,0 +1,36 @@
+import { connect } from "react-redux";
+import { logout } from "../../actions/session_actions";
+import { getUser, changeUser } from "../../actions/user_actions";
+import {
+  findLocations,
+  clearLocations,
+  findLocation
+} from "../../actions/location_actions";
+import Edit from "./edit";
+
+const msp = (state, ownProps) => {
+  return {
+    currentUser: state.session.currentUser,
+    locations: state.entities.locations,
+    errors: state.errors,
+    currentLocation: state.entities.locations,
+    userId: ownProps.match.params.userId,
+    user: state.entities.user
+  };
+};
+
+const mdp = dispatch => {
+  return {
+    logout: () => dispatch(logout()),
+    findLocations: string => dispatch(findLocations(string)),
+    clearLocations: () => dispatch(clearLocations()),
+    findLocation: id => dispatch(findLocation(id)),
+    getUser: id => dispatch(getUser(id)),
+    changeUser: user => dispatch(changeUser(user))
+  };
+};
+
+export default connect(
+  msp,
+  mdp
+)(Edit);

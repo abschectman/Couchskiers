7a1fe62a824bffeacc6c11bab834ce5cc1c895a8
abschectman <abschectman@gmail.com>
2019-10-31 16:36:34 -0400

    Bucket restructering

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.active_storage.service = :amazon_dev
-  config.active_storage.service = :amazon_prod
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker

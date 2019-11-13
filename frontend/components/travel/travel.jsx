0da1332e0bfda9327bb103dc718ab8ffa2e4d628
Bilal <bilalxabdullah@gmail.com>
2019-11-13 09:23:00 -0500

    Added travel comonent

---
+++ frontend/components/travel/travel.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+
+class Travel extends React.Component{
+  constructor(){
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  render(){
+    <div>
+
+      <div id="travel-header">
+          <span>EXPLORE EVERY MOUNTAIN WITH COUCHSKIERS</span>
+      </div>
+        <div id="travel-links">
+            <a id="travel-image-one"></a>
+            <a id="travel-image-two"></a>
+            <a id="travel-image-three"></a>
+        </div>
+      <div id="travel-search">
+        
+      </div>
+    </div>
+  }
+}
+
+export default Travel;

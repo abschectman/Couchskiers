548c9507e7f92131a68bcc7a6b4ae892112b18fa
abschectman <abschectman@gmail.com>
2019-11-14 13:10:43 -0500

    Removed logger for production readiness

--- frontend/couch.jsx
+++ frontend/couch.jsx
@@ -28,7 +28,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
   } else {
     store = configureStore();
   }
-
-  window.state = store.getState
   ReactDOM.render(<Root store={store}/>, document.getElementById("root"))
 })
